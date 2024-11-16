8f869bdbde1fc86b366431d9aefc55ef9f96936d
Ashu <253385+Ashu@users.noreply.github.com>
2024-11-16 15:14:49 +0530

    Initial Commit

---
+++ NewsReader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NewsReader
+//
+//  Created by Ashu on 16/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NewsReader/NewsReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsReaderApp.swift
+//  NewsReader
+//
+//  Created by Ashu on 16/11/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
